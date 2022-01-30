734637a8cf24091adbc665b479842ec0ab70c617
zhongshaofa <zhongshaofa@shein.com>
2022-01-30 11:37:38 +0800

    [feat]完善

--- src/utils/request.js
+++ src/utils/request.js
@@ -46,15 +46,15 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.error_code !== 0) {
       Message({
-        message: res.message || 'Error',
+        message: res.error_msg || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.error_code === 10000 || res.error_code === 10001) {
         // to re-login
         MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
           confirmButtonText: 'Re-Login',
@@ -66,7 +66,7 @@ service.interceptors.response.use(
           })
         })
       }
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(res.error_msg || 'Error'))
     } else {
       return res
     }
