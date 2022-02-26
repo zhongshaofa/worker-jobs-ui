b0eb5000a7c5c9bcc13d795edab2f264775302d6
zhongshaofa <zhongshaofa@shein.com>
2022-02-26 16:03:24 +0800

    [feat]完善

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,9 +16,6 @@ service.interceptors.request.use(
     // do something before request is sent
 
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
       config.headers['token'] = getToken()
       config.headers['Access-Control-Allow-Origin'] = '*'
       config.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE'
